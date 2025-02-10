e155f1356cc3920c0a840507ca0decf11df78dbc
DukaSiqueira <e.siqueira@edu.unipar.br>
2025-02-10 09:39:49 -0300

    Remove unused middleware import from API routes

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\UserController;
-use App\Http\Middleware\HandleApiExceptions;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource("users", UserController::class);
